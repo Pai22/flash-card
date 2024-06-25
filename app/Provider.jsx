ff41be2bc354119d830309f7580ebebcbcefd9f0
imkuromi <jarawee111144@gmail.com>
2024-06-25 12:12:57 +0700

    change

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -3,7 +3,7 @@ import {NextUIProvider} from "@nextui-org/react";
 export default function Provider({children}) {
   return (
     <NextUIProvider>
-      <main className="bg-sky-100 min-h-screen flex flex-col items-center justify-start">
+      <main className="bg-sky-100 min-h-screen">
         {children}
       </main>
     </NextUIProvider>
